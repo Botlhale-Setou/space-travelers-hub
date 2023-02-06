623357157fb7f0f13e5337a491e60227ac13de79
Botlhale Setou <botlhale.setou@gmail.com>
2023-02-06 19:40:18 +0200

    Install dependancies, create empty MyProfile & Rockets basic struct

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Rockets from "./pages/Rockets";
+import MyProfile from "./pages/MyProfile";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path='/' element={<Rockets />} />
+      <Route path='/myprofile' element={<MyProfile />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
